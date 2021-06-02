1ec96313b09c5f3cabe6f01e6b505c6a4162b1f4
josiane dufour <josiane@elder.org>
2021-06-02 14:18:10 +0100

    add logic to handle a name query parameter

--- app.ts
+++ app.ts
@@ -12,15 +12,31 @@ console.log(`Server is now running on: ttp://${HOSTNAME}:${PORT}`);
 
 // return html
 for await (const req of server) {
-    req.respond({ 
-        body: "<h1>Hello World</h1>",
-    });
+    // req.url holds the URL fragment = everything that comes after teh address and the port
+    // you can use the browser URLSearchParams object to parse out query string params
+    // take the substrig because a / gets appended by the browser
+    const searchParameters = new URLSearchParams(req.url.substring(1));
+    const name = searchParameters.get("name")
+
+
+    // check to see if the name was passed in
+    // if not, throw a 500 error
+    if (name) {
+        req.respond({ 
+            body: "<h1>Hello World</h1>",
+        });
+    } else {
+        req.respond({ 
+            status: 500,
+            body: "<p>The name parameter was not found on the request</p>",
+        });
+    }
+  
 }
 
-// run with deno run app.ts
-// need permissions so add flag --allow-net
-// deno run --allow-net=0.0.0.0:3001 app.ts
 
-// our server is now running!
+// deno run --allow-net=0.0.0.0:3001 app.ts
+// http://localhost:3000?name=World
+
 
 

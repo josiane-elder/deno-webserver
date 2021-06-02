9bc87b2cbafd3251c339964ae7e21213ca669a84
josiane dufour <josiane@elder.org>
2021-06-02 14:51:57 +0100

    handle route params

--- app.ts
+++ app.ts
@@ -1,4 +1,25 @@
+  // how to parse route params such as /:name ?
+    // there is a node package (route-parser) that can do this for us
+    // but how to get this in deno? (where's the url)??
+
+    // if a package is isomorphic (works in the browser) 
+    // should be able to use it with deno
+
+    // there are some services that allow to import a package from npm via URL
+    // this won't work in deno because it uses commonJS syntax 
+    // is module.exports = Route;
+    // deno only supports es modules
+
+    // tere is a service called jpsm that takes a module by url and gives a esmodule compatible import/export
+
+
+import routeParser from "https://dev.jspm.io/route-parser@0.0.5";
+// inport ts types for route parser
+import RouteParser from "https://unpkg.com/@types/route-parser@0.1.3/index.d.ts";
 import { serve } from 'https://deno.land/std/http/server.ts';
+
+const Route = routeParser as typeof RouteParser;
+
 const PORT = 3001;
 const HOSTNAME = "0.0.0.0";
 
@@ -10,25 +31,22 @@ const server = serve({ hostname: HOSTNAME, port: PORT });
 // start the server and listen for incoming requests
 console.log(`Server is now running on: ttp://${HOSTNAME}:${PORT}`);
 
+const route = new Route("/:name");
+
 // return html
 for await (const req of server) {
-    // req.url holds the URL fragment = everything that comes after teh address and the port
-    // you can use the browser URLSearchParams object to parse out query string params
-    // take the substrig because a / gets appended by the browser
-    const searchParameters = new URLSearchParams(req.url.substring(1));
-    const name = searchParameters.get("name")
-
+  
+    const match: any = route.match(req.url);
 
     // check to see if the name was passed in
     // if not, throw a 500 error
-    if (name) {
+    if (match.name) {
         req.respond({ 
-            body: "<h1>Hello World</h1>",
+            body: `Hello, ${match.name}`,
         });
     } else {
         req.respond({ 
-            status: 500,
-            body: "<p>The name parameter was not found on the request</p>",
+            body: "Please pass a route name.",
         });
     }
   
